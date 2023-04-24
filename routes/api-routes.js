7a02a5cf9706c0484ab54755bd1d357bedf8a046
eric <123092979+etranchell@users.noreply.github.com>
2023-04-24 17:30:57 -0400

    fix routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 router.get('/api/notes', async (req, res) => {
     const dbJson = await JSON.parse(fs.readFileSync('develop/db/db.json', 'utf-8'));
@@ -11,10 +12,22 @@ router.post('/api/notes', (req, res) => {
     const newFeedback = {
         title: req.body.title,
         text: req.body.text,
+        id: uuidv4(),
     };
     dbJson.push(newFeedback);
     fs.writeFileSync('develop/db/db.json', JSON.stringify(dbJson));
     res.json(dbJson);
 });
+
+
+router.delete('/api/notes/:id', (req, res) => {
+    let data = fs.readFileSync("develop/db/db.json", "utf8");
+    const dataJSON =  JSON.parse(data);
+    const newNotes = dataJSON.filter((note) => { 
+      return note.id !== req.params.id;
+    });
+    fs.writeFileSync("develop/db/db.json",JSON.stringify(newNotes));
+    res.json("Note deleted.");
+  });
 
 module.exports = router;
