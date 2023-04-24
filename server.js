ffd95e3bba900fcd140446e03ad85795e603190b
eric <123092979+etranchell@users.noreply.github.com>
2023-04-24 12:52:29 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.watchFile);
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
